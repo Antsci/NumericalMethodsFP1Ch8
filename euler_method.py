1c32f2a87c6e83c70755c174846b0ac9045597e8
Isaac <isaacantscherl@gmail.com>
2021-01-30 14:01:28 +0000

    added mid point method

--- euler_method.py
+++ euler_method.py
@@ -1,5 +1,5 @@
 from math import *
-def euler_ode(derivative: str, x: float, y: float, iter_num: int, end_point: float)  -> float:
+def euler_ode_method(derivative: str, x: float, y: float, iter_num: int, end_point: float)  -> float:
     iter_num = int(iter_num)
     h = (end_point-x)/iter_num
     derivative = derivative.replace('^','**')
@@ -12,6 +12,30 @@ def euler_ode(derivative: str, x: float, y: float, iter_num: int, end_point: flo
         y += (h*num_der)
         x += h
     print(f"f({x}) = {y}")
+
+
+def midpoint_method(derivative: str, x: float, y: float, step_length: float, end_point: float)  -> float:
+    derivative = derivative.replace('^','**')
+    derivative = derivative.replace('ln(','log(')
+    n = 0
+    print(f"(x_{n},y_{n}) = {x,y}")
+    num_der = eval(derivative)
+    print(f"derivative = {num_der}")
+    print(f"y_{n+1} = y_{n} + (h * derivative) = {y + (step_length*num_der)}")
+    y_prev = y
+    y += (step_length*num_der)
+    x += step_length
+    for n in range(int((end_point-x)/step_length)):
+        print(f"(x_{n},y_{n}) = {x,y}")
+        num_der = eval(derivative)
+        print(f"derivative = {num_der}")
+        print(f"y_{n+1} = y_{n-1} + (2*h * derivative) = {y + (2*step_length*num_der)}")
+        hold = y
+        y = y_prev + (2*step_length*num_der)
+        y_prev = hold
+        x += step_length
+    print(f"f({x}) = {y}")
+
 der = input("derivative: str ")
-args = [float(item) for item in input("x: float, y: float, iter_num: int, end_point: float ").split()] 
-euler_ode(der, *args)
+args = [float(item) for item in input("x: float, y: float, iter_num/step_length: int/float, end_point: float ").split()] 
+midpoint_method(der, *args)
