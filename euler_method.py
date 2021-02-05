ad49b3d3e3339cf10dc9bbb611e9a937044d9fe2
Isaac <isaacantscherl@gmail.com>
2021-02-05 11:10:29 +0000

    added simpsons rule

--- euler_method.py
+++ euler_method.py
@@ -1,5 +1,5 @@
 from math import *
-def euler_ode_method(derivative: str, x: float, y: float, iter_num: int, end_point: float)  -> float:
+def euler_ode_method(derivative: str, x: float, y: float, iter_num: int, end_point: float) -> float:
     iter_num = int(iter_num)
     h = (end_point-x)/iter_num
     derivative = derivative.replace('^','**')
@@ -14,7 +14,7 @@ def euler_ode_method(derivative: str, x: float, y: float, iter_num: int, end_poi
     print(f"f({x}) = {y}")
 
 
-def midpoint_method(derivative: str, x: float, y: float, step_length: float, end_point: float)  -> float:
+def midpoint_method(derivative: str, x: float, y: float, step_length: float, end_point: float) -> float:
     derivative = derivative.replace('^','**')
     derivative = derivative.replace('ln(','log(')
     n = 0
@@ -36,6 +36,20 @@ def midpoint_method(derivative: str, x: float, y: float, step_length: float, end
         x += step_length
     print(f"f({x}) = {y}")
 
-der = input("derivative: str ")
-args = [float(item) for item in input("x: float, y: float, iter_num/step_length: int/float, end_point: float ").split()] 
-midpoint_method(der, *args)
+def second_order(derivative: str, x: float, y: float, iter_num: int, end_point: float) -> float:
+    pass
+def simpsons(func: str, start: float, end:float, strips: int) -> float:
+    h = (end - start)/strips
+    table = [eval(func.replace('x','x/10')) for x in range(int((start)*10), int((end+h)*10), int(h*10))]
+    print(table)
+    ends = [table[0], table[-1]]
+    table = table[1:-1]
+    print(table)
+    print([table[i] for i in range(len(table)) if i%2==0])
+    print(f"(1/3) * {h} * (({ends[0]+ends[1]}) + 4 * {fsum([table[i] for i in range(len(table)) if i%2==0])} + 2 * {fsum([table[i] for i in range(len(table)) if i%2!=0])})")
+    answer = (1/3) * h * ((ends[0]+ends[1]) + 4 * fsum([table[i] for i in range(len(table)) if i%2==0]) + 2 * fsum([table[i] for i in range(len(table)) if i%2!=0]))
+    return answer
+# der = input("derivative: str ")
+# args = [float(item) for item in input("x: float, y: float, iter_num/step_length: int/float, end_point: float ").split()] 
+# midpoint_method(der, *args)
+print(simpsons("log(x)/(x)", 2, 4, 4))
